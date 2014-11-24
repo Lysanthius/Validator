2f8dc83257c6e2f9c094320b4fe5c50d446e7f09
Amélie <avw@neuf.fr>
2014-11-24 09:28:49 +0100

    Array Validator done

--- tests/ArrayValidatorTest.php
+++ tests/ArrayValidatorTest.php
@@ -2,7 +2,81 @@
 
 namespace Tests\AVW\Validator;
 
+use AVW\Validator\ArrayValidator;
+
+/**
+ * Class ArrayValidatorTest
+ * @package Tests\AVW\Validator
+ */
 class ArrayValidatorTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Check if the array is empty
+     *
+     * @throws \Exception
+     */
+    public function testIsEmpty()
+    {
+        $array = [];
 
+        $result = ArrayValidator::isEmpty($array);
+
+        $this->assertEquals($result, true);
+    }
+
+    /**
+     * Check if the array has this length
+     *
+     * @throws \Exception
+     */
+    public function testCompare()
+    {
+        $array = ['pomme' => 'verte', 'poire' => 'jaune'];
+
+        $result = ArrayValidator::compare($array, 2, ArrayValidator::COMPARE_EQUAL);
+
+        $this->assertEquals($result, true);
+    }
+
+    /**
+     * Check if the array's length is between the two numbers
+     *
+     * @throws \Exception
+     */
+    public function testNumberElementsBetween()
+    {
+        $array = ['pomme' => 'verte', 'poire' => 'jaune', 'orange' => 'orange'];
+
+        $result = ArrayValidator::numberElementsBetween($array, 1, 4);
+
+        $this->assertEquals($result, true);
+    }
+
+    /**
+     * Check if the key is in the array
+     *
+     * @throws \Exception
+     */
+    public function testKeyExists()
+    {
+        $array = ['chocolat' => 'noir'];
+
+        $result = ArrayValidator::keyExists($array, 'chocolat');
+
+        $this->assertEquals($result, true);
+    }
+
+    /**
+     * Check if the value is in the array
+     *
+     * @throws \Exception
+     */
+    public function testValueExists()
+    {
+        $array = ['chocolat' => 'blanc'];
+
+        $result = ArrayValidator::valueExists($array, 'blanc');
+
+        $this->assertEquals($result, true);
+    }
 } 
